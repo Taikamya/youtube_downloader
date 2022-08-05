6ef725a06f6efb13311fc1da99419f469cd61c70
Dimitri Dias Moreira <taikamya@outlook.com>
2022-08-05 15:01:58 +0200

    secure url check

--- main.py
+++ main.py
@@ -2,29 +2,39 @@ from pytube import YouTube
 from sys import argv
 from settings import URL
 
-# getting the YT link from input
+# soft-coded link
 # url = input("What's the YT URL? \n")
 
-# hard-coded YT link -- ONLY USE FOR TESTING --
+# hard-coded link -- ONLY USE FOR TESTING --
 url = URL
 
-# YT link from first argument in the CLI
+# link from first argument in the CLI
 # url = argv[1]
 
-yt_obj = YouTube(url)
-
-downloader_obj = yt_obj.streams.get_highest_resolution()
+def secure_url() -> str:
+    '''Secure URL check function'''
+    if url[0:5] == "http:":
+        s_url = url.replace("http", "https")
+    else:
+        s_url = url
+    return s_url
 
 def download(link) -> None:
+    '''Download function'''
     if downloader_obj != None:
         print("Video found. Begin downloading...")
     else:
         print("Video not found. Invalid link.")
     link.download("./downloads/")
 
+# YT link
+yt_obj = YouTube(secure_url())
+# Highest resolution available
+downloader_obj = yt_obj.streams.get_highest_resolution()
+
 
 if __name__=="__main__":
-    # Testing purposes
+    # Link info
     print(f"Title: {yt_obj.title}\nViews: {yt_obj.views}")
     # ---------------------------- // ----------------------------
     # Download if all ok
